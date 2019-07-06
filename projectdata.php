e5382dbcd3a1268fe3e2c79034e77670629e2271
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-06 18:12:55 -0500

    new: added a few changes in the presentation, fixing variable meetings name and change the first page to index.

--- projectdata.php
+++ projectdata.php
@@ -1,40 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <title>projectdata.html</title>
- <link rel = "stylesheet"
-  type = "text/css"
-  href = "contact.css" />
+  <meta charset="UTF-8">
+  <title>projectdata.html</title>
+  <link rel = "stylesheet" type = "text/css" href = "contact.css" />
 </head>
 <body>
- <?php
-date_default_timezone_set('America/Chicago');
+<?php
+  
+  date_default_timezone_set('America/Chicago');
 
  //read data from form
- $blazerId = filter_input(INPUT_GET, "blazerId");
- $teamNumber = filter_input(INPUT_GET, "teamNumber");
- $totalTime = filter_input(INPUT_GET, "totalTime");
- $analysisAndDesign = filter_input(INPUT_GET, "analysisAndDesign");
- $coding = filter_input(INPUT_GET, "coding");
- $testing = filter_input(INPUT_GET, "testing");;
- $meeting = filter_input(INPUT_GET, "meeting");;
- $other = filter_input(INPUT_GET, "other");;
+  $blazerId = filter_input(INPUT_GET, "blazerId");
+  $teamNumber = filter_input(INPUT_GET, "teamNumber");
+  $totalTime = filter_input(INPUT_GET, "totalTime");
+  $analysisAndDesign = filter_input(INPUT_GET, "analysisAndDesign");
+  $coding = filter_input(INPUT_GET, "coding");
+  $testing = filter_input(INPUT_GET, "testing");
+  $meeting = filter_input(INPUT_GET, "meeting");
+  $other = filter_input(INPUT_GET, "other");
+
  //print form results to user
  print <<< HERE
- <h1>Thanks!</h1>
+ <h1>Thank you!</h1>
  <p>
-  Your spam will be arriving shortly.
+  Your record was registered correctly.
  </p>
  <p>
  Blazer ID: $blazerId <br />
  Total Time: $totalTime <br />
  Analysis and Design %: $analysisAndDesign <br />
- Coding %: $coding
- Testing %: $testing
- Meetings %: $meeting
- Other %: $other
+ Coding %: $coding <br />
+ Testing %: $testing <br />
+ Meetings %: $meeting <br />
+ Other %: $other <br />
  </p>
+ <p><a href="index.html">Home</a></p>
 HERE;
  //open file for output
  $csvFileOpen = fopen("projectdata.csv", "a");
@@ -50,25 +51,24 @@ HERE;
    'analysisAndDesign' => $analysisAndDesign,
    'coding' => $coding,
    'testing' => $testing,
-   'meetings' => $meetings,
+   'meetings' => $meeting,
    'other' => $other,
    'timestamp' =>  $date = date('Y-m-d H:i:s')
   );
- //write to the file
- fputcsv($csvFileOpen, $form_data);
+  //write to the file
+  fputcsv($csvFileOpen, $form_data);
 
- echo "<html><body><table>\n\n";
- $f = fopen("projectdata.csv", "r");
- while (($line = fgetcsv($f)) !== false) {
+  echo "<html><body><table>\n\n";
+  $f = fopen("projectdata.csv", "r");
+  while (($line = fgetcsv($f)) !== false) {
          echo "<tr>";
          foreach ($line as $cell) {
                  echo "<td>" . htmlspecialchars($cell) . "</td>";
          }
          echo "</tr>\n";
- }
- fclose($f);
- echo "\n</table></body></html>";
- 
- ?>
+  }
+  fclose($f);
+  echo "\n</table></body></html>";
+?>
 </body>
 </html>
